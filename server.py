b7ca132cb4641954fe17b4a55aa806a0dd9f0929
AxelAlza <axelalza1998@gmail.com>
2020-02-19 01:03:56 -0300

    Lmao

--- server.py
+++ server.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='/static',
-            template_folder='/templates')
+app = Flask(__name__)
 
 
 @app.route('/')
 def Inicio():
-    return render_template("Inicio.html")
+    return render_template("/Inicio.html")
 
 
 @app.route('/Mantenimiento/Clientes')
 def MantClientes():
-    return render_template("MantClientes.html")
+    return render_template("/MantClientes.html")
 
 
 @app.route('/Mantenimiento/Empleados', methods=['GET'])
 def MantEmpleados():
-    return render_template("MantEmpleados.html")
+    return render_template("/MantEmpleados.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.jinja_env.auto_reload = True
+    app.jinja_env.cache = {}
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
