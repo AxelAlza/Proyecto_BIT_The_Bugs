f5b4c92651c61a21b143b04a29431ef36c126c8b
AxelAlza <axelalza1998@gmail.com>
2020-02-27 03:55:16 -0300

    Lmao

--- server.py
+++ server.py
@@ -1,4 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+
+from werkzeug.utils import redirect
+
+dbconnect = sqlite3.connect('The_Bugs.db', check_same_thread=False)
+database = dbconnect.cursor()
+
+
+def htmlfy(tabla):
+    html = ""
+    database.execute("Select * From " + tabla)
+    lista = database.fetchall()
+    for tupla in lista:
+        tup = "<tr>"
+        for columna in tupla:
+            td = "<td>" + str(columna) + "</td>"
+            tup = tup + td
+        tup = tup + "</tr>"
+        html = html + tup
+    return html
+
 
 app = Flask(__name__)
 
@@ -10,17 +31,34 @@ def Inicio():
 
 @app.route('/Mantenimiento/Clientes')
 def MantClientes():
-    return render_template("/MantClientes.html")
+    html = htmlfy("Cliente")
+    return render_template("/MantClientes.html", html=html)
 
 
 @app.route('/Mantenimiento/Empleados')
 def MantEmpleados():
-    return render_template("/MantEmpleados.html")
+    html = htmlfy("Empleado")
+    return render_template("/MantEmpleados.html", html=html)
 
 
-@app.route('/Mantenimiento/Empleados/AgregarEmpleado')
+@app.route('/Mantenimiento/Empleados/AgregarEmpleado', methods=['GET', 'POST'])
 def AgregarEmpleado():
-    return render_template("/AgregarEmpleado.html")
+    if request.method == "POST":
+        EmpCi = request.form['EmpCi']
+        EmpNom = request.form['EmpNom']
+        EmpApe = request.form['EmpApe']
+        EmpAge = request.form['EmpAge']
+        EmpTel = request.form['EmpTel']
+        EmpDir = request.form['EmpDir']
+        EmpMail = request.form['EmpMail']
+        EmpHde = request.form['EmpHde']
+        EmpHha = request.form['EmpHha']
+        database.execute(
+            "INSERT INTO Empleado VALUES (? , ? , ? , ? , ?, ? ,? ,? , ?)", (EmpCi, EmpNom, EmpApe, EmpAge, EmpTel
+                                                                             , EmpDir, EmpMail, EmpHde, EmpHha))
+        dbconnect.commit()
+        return redirect("/Mantenimiento/Empleados")
+    return render_template('/AgregarEmpleado.html')
 
 
 if __name__ == "__main__":
--- static/js/funciones.js
+++ static/js/funciones.js
@@ -1,5 +1,7 @@
 $("#table tr").click(function(){
    $(this).addClass('table-primary').siblings().removeClass('table-primary');
-   var value=$(this).find('td:last').html();
+   var cedula=$(this).find('td:first').html();
 });
+
+
 
