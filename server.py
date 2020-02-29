b7c5ec279bb595f04a257b66e6bfcc3d7c15abe8
AxelAlza <axelalza1998@gmail.com>
2020-02-29 16:59:17 -0300

    Lmao

--- server.py
+++ server.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template, request, jsonify
 import sqlite3
 
-from werkzeug.utils import redirect
+from flask import Flask, render_template, request, jsonify
 
 dbconnect = sqlite3.connect('The_Bugs.db', check_same_thread=False)
 database = dbconnect.cursor()
 
 
-def htmlfy(tabla):
-    html = ""
+def htmlfy(tabla, keys):
     database.execute("Select * From " + tabla)
     lista = database.fetchall()
-    for tupla in lista:
-        tup = "<tr>"
+    itemj = ''
+    i = 0
+    for num, tupla in enumerate(lista):
+        agr2 = ','
+        json = "{"
+        agr = ','
         for columna in tupla:
-            td = "<td>" + str(columna) + "</td>"
-            tup = tup + td
-        tup = tup + "</tr>"
-        html = html + tup
-    return html
+            if i == len(keys):
+                i = 0
+            if i == len(keys) - 1:
+                agr = ''
+            column = keys[i] + '"' + str(columna) + '"' + agr
+            json = json + column
+            i = i + 1
+        if num == len(lista) - 1:
+            agr2 = ''
+        itemj = itemj + json + '}' + agr2
+    itemj = "[" + itemj + "]"
+    return itemj
 
 
 app = Flask(__name__)
@@ -29,24 +38,60 @@ def Inicio():
     return render_template("/Inicio.html")
 
 
-@app.route('/Mantenimiento/Clientes')
+@app.route('/Mantenimiento/getpagesemp', methods=['GET'])
+def getpagesemp():
+    keys = ['"empci":', '"empnom":', '"empape":', '"empage":', '"emptel":', '"empdir":', '"empmail":', '"emphde":',
+            '"emphha":']
+    htmlpages = htmlfy("Empleado", keys)
+    return htmlpages
+
+
+@app.route('/Mantenimiento/getpagescli', methods=['GET'])
+def getpagescli():
+    keys = ['"clici":', '"clinom":', '"cliape":', '"cliage":', '"clitel":', '"clidir":', '"clihid']
+    htmlpages = htmlfy("Cliente", keys)
+    return htmlpages
+
+
+@app.route('/Mantenimiento/Clientes', methods=['GET'])
 def MantClientes():
-    html = htmlfy("Cliente")
-    return render_template("/MantClientes.html", html=html)
+    return render_template("/MantClientes.html")
 
 
 @app.route('/Mantenimiento/Empleados')
 def MantEmpleados():
-    html = htmlfy("Empleado")
-    return render_template("/MantEmpleados.html", html=html)
+    return render_template("/MantEmpleados.html")
 
 
-@app.route('/pass_val', methods=['POST'])
-def pass_val():
+@app.route('/EliminarEmpleado', methods=['POST'])
+def EliminarEmpleado():
     cedula = request.args.get('cedula')
     database.execute('Delete from empleado where EmpCi = ?', (cedula,))
     dbconnect.commit()
     return jsonify({'reply': "success"})
+
+
+@app.route('/EliminarCliente', methods=['POST'])
+def EliminarCliente():
+    cedula = request.args.get('cedula')
+    database.execute('Delete from Cliente where EmpCi = ?', (cedula,))
+    dbconnect.commit()
+    return jsonify({'reply': "success"})
+
+
+@app.route('/CedulaDisponibleEmp', methods=['GET', 'POST'])
+def CedulaDisponibleEmp():
+    cedula = request.args.get('cedula')
+    modo = request.args.get('modo')
+    if modo == "emp":
+        database.execute('Select EmpCi  from Empleado where EmpCi = ?', (cedula,))
+    else:
+        database.execute('Select CliCi  from Cliente where CliCi = ?', (cedula,))
+    ci = database.fetchone()
+    if ci is None:
+        return jsonify({'reply': "success"})
+    else:
+        return jsonify({'reply': "fail"})
 
 
 @app.route('/Mantenimiento/Empleados/AgregarEmpleado', methods=['GET', 'POST'])
@@ -65,8 +110,7 @@ def AgregarEmpleado():
             "INSERT INTO Empleado VALUES (? , ? , ? , ? , ?, ? ,? ,? , ?)", (EmpCi, EmpNom, EmpApe, EmpAge, EmpTel
                                                                              , EmpDir, EmpMail, EmpHde, EmpHha))
         dbconnect.commit()
-        return redirect("/Mantenimiento/Empleados")
-    return render_template('/AgregarEmpleado.html')
+    return render_template("/AgregarEmpleado.html")
 
 
 if __name__ == "__main__":
