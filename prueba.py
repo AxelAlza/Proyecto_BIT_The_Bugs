d57ee8d4443b0a4a9322e8311f644d5bb7aacebe
AxelAlza <56464381+AxelAlza@users.noreply.github.com>
2020-04-01 02:40:19 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,7 +3,7 @@ import os
 
 
 def key():
-    os.environ['GOOGLE_API'] = 'AIzaSyBVIfuWRfCpm7l0qGw8KSaxD6YB4vGxSxo'
+    os.environ['GOOGLE_API'] = 'Secret'
     env_var = os.environ.get('GOOGLE_API')
     return env_var
 
